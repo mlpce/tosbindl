ac15f25033da32a018b166e33d65b86281fe6045
mlpce <154090656+mlpce@users.noreply.github.com>
2025-05-09 22:32:06 +0100

    In unit test crawcin.lua check with a file forced to conin

--- src/gemdos/unittest/crawcin.lua
+++ src/gemdos/unittest/crawcin.lua
@@ -1,3 +1,5 @@
+local force_standard_handle = require("fcestdhd")
+
 -- Unittest for gemdos.Crawcin
 gemdos.Cconws("Test gemdos.Crawcin\r\n")
 
@@ -64,6 +66,56 @@ assert(ascii == 0, "Wrong ascii code: " .. ascii)
 assert(scan_code == 30, "Wrong scan code: " .. scan_code)
 assert(shift == 8, "Wrong shift: " .. shift)
 gemdos.Cconws("\r\n");
+
+-- Check with a file forced to conin
+gemdos.Cconws("Check file forced to conin\r\n")
+
+local fn = function()
+  local ascii, scancode, shift = gemdos.Crawcin()
+
+  -- First character read must be '0'
+  assert(ascii == 48, "Wrong ASCII code: " .. ascii .. " expected 48")
+
+  -- scancode and shift must be zero
+  assert(scancode == 0)
+  assert(shift == 0)
+
+  ascii, scancode, shift = gemdos.Crawcin()
+
+  -- Second character read must be '0'
+  assert(ascii == 49, "Wrong ASCII code: " .. ascii .. " expected 49")
+
+  -- scancode and shift must be zero
+  assert(scancode == 0)
+  assert(shift == 0)
+
+  ascii, scancode, shift = gemdos.Crawcin()
+
+  -- Third character read must be '0'
+  assert(ascii == 50, "Wrong ASCII code: " .. ascii .. " expected 50")
+
+  -- scancode and shift must be zero
+  assert(scancode == 0)
+  assert(shift == 0)
+end
+
+-- Write 012 to coninfce.txt
+local ec, fud <close> = gemdos.Fcreate("coninfce.txt", gemdos.const.Fattrib.none)
+assert(ec == 0)
+
+ec = fud:writes("012")
+assert(ec == 3)
+
+ec = fud:close()
+assert(ec == 0)
+
+-- Force coninfce.txt to conin and call fn
+local result, err = force_standard_handle.ForcedFilenameCall(gemdos.const.Fdup.conin,
+  "coninfce.txt", fn)
+assert(result, err)
+
+-- Delete coninfce.txt
+gemdos.Fdelete("coninfce.txt")
 
 -- Completed
 gemdos.Cconws("Test gemdos.Crawcin completed\r\n")
