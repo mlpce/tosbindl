da66f43f72d3daf8a34108a543b5ae75e26e0846
mlpce <154090656+mlpce@users.noreply.github.com>
2025-05-14 19:25:35 +0100

    Update unit test fforce.lua

--- src/gemdos/unittest/fcestdhd.lua
+++ src/gemdos/unittest/fcestdhd.lua
@@ -1,7 +1,16 @@
 local ForceStdHandle = {}
 
+local allowed_handles = {}
+for k,v in pairs(gemdos.const.Fdup) do
+  allowed_handles[v] = k
+end
+
 -- Calls passed function with standard handle forced to the filename
 function ForceStdHandle.ForcedFilenameCall(standard_handle, filename, fn)
+  if not allowed_handles[standard_handle] then
+    error("Invalid standard handle", 2)
+  end
+
   -- Duplicate original standard handle
   local dup_ec, dupfud <close> = gemdos.Fdup(standard_handle);
   if dup_ec ~= 0 then
@@ -30,6 +39,19 @@ function ForceStdHandle.ForcedFilenameCall(standard_handle, filename, fn)
   ec = gemdos.Fforce(dupfud, standard_handle)
   if ec ~= 0 then
     gemdos.Cconws("Could set standard handle back to original handle")
+  end
+
+  ec = dupfud:close()
+  if ec ~= 0 then
+    gemdos.Cconws("force: dupfud:close() returned " .. ec .. "\r\n")
+  end
+
+  -- fud may have been detached by fn
+  if fud:handle() ~= 0 then
+    ec = fud:close()
+    if ec ~= 0 then
+      gemdos.Cconws("force: fud:close() returned " .. ec .. "\r\n")
+    end
   end
 
   -- Return results
--- src/gemdos/unittest/fforce.lua
+++ src/gemdos/unittest/fforce.lua
@@ -1,60 +1,39 @@
+local force_standard_handle = require("fcestdhd")
+
 -- Test gemdos.Fforce
 gemdos.Cconws("Test gemdos.Fforce\r\n")
 
--- TODO(mlpce): Investigate ENHNDL when looping unittest\fforce.lua
--- If this test is repeated in a loop, eventually it will fail with ENHNDL.
--- This is the case with both EmuTOS 1.3 and TOS 1.04 (in TOS 1.04 you will
--- likely need FOLDR100.PRG otherwise the system will run out of resources
--- before ENHDNL can be generated).
--- If the file_fud:handle() is printed out it can be seen that the handle
--- is increasing each time the test is run.
-
 -- Create output file
-local ec, file_fud = gemdos.Fcreate("REDIRECT", gemdos.const.Fattrib.none)
+local ec, fud <close> = gemdos.Fcreate("conoutfc.txt", gemdos.const.Fattrib.none)
+gemdos.Cconws("fud handle " .. fud:handle() .. "\r\n")
 assert(ec == 0)
-assert(file_fud:handle() >= 6)
-gemdos.Cconws("file_fud:handle() " .. file_fud:handle() .. "\r\n")
+fud:close()
 
--- Duplicate conout
-local duplicate_conout_fud
-ec, duplicate_conout_fud = gemdos.Fdup(gemdos.const.Fdup.conout)
-assert(ec == 0)
+local fn = function(standard_handle, fud)
+  -- Pexec0 some output
+  local pexec0_ec = gemdos.Pexec0("lua.ttp", { "-e print(\"HELLO\")" } )
+  if pexec0_ec >= 0 then
+    -- fud will have been closed when child exited
+    fud:detach()
+  end
+  assert(pexec0_ec == 0)
+end
 
--- Force conout to file
-ec = gemdos.Fforce(file_fud, gemdos.const.Fdup.conout)
-assert(ec == 0)
-
--- Pexec0 some output
-local pexec0_ec = gemdos.Pexec0("lua.ttp", { "-e print(\"HELLO\")" } )
-
--- Restore conout
-ec = gemdos.Fforce(duplicate_conout_fud, gemdos.const.Fdup.conout)
-assert(ec == 0)
-
--- Close duplicate
-ec = gemdos.Fclose(duplicate_conout_fud)
-assert(ec == 0)
-
--- NOTE(mlpce): Close output file fails with EINTRN after a Pexec0. If
--- the call to Pexec0 is removed then this close will succeed. However
--- looping the test (with the output check below disabled) will still
--- eventually fail with ENHNDL.
-ec = gemdos.Fclose(file_fud)
-assert(pexec0_ec == 0)
-assert(ec == gemdos.const.Error.EINTRN)
+-- Force coninfce.txt to conin and call fn
+local result, err = force_standard_handle.ForcedFilenameCall(gemdos.const.Fdup.conout,
+  "conoutfc.txt", fn)
+assert(result, err)
 
 -- Open the output for checking
-local check_fud
-ec, check_fud = gemdos.Fopen("REDIRECT", gemdos.const.Fopen.readonly)
+local ec, check_fud <close> = gemdos.Fopen("conoutfc.txt", gemdos.const.Fopen.readonly)
 assert(ec == 0)
 
 -- Read string
-local str
-ec, str = gemdos.Freads(check_fud, 100)
+local ec, str = gemdos.Freads(check_fud, 100)
 assert(ec == 7 and str == "HELLO\r\n")
 
 -- Close
-ec = gemdos.Fclose(check_fud)
+ec = check_fud:close()
 assert(ec == 0)
 
 gemdos.Cconws("Test gemdos.Fforce completed\r\n")
