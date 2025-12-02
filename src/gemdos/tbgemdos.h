b7b19498e15f34a6a4eef9390a17084f784d8b51
mlpce <154090656+mlpce@users.noreply.github.com>
2025-12-02 20:29:54 +0000

    Rename TOSBINDL_GEMDOSLIBNAME to TOSBINDL_GEMDOS_LIBNAME

--- src/gemdos/tbgemdos.h
+++ src/gemdos/tbgemdos.h
@@ -1,7 +1,7 @@
 #ifndef TBGEMDOS_HEADER_INCLUDED
 #define TBGEMDOS_HEADER_INCLUDED
 
-#define TOSBINDL_GEMDOSLIBNAME "gemdos"
+#define TOSBINDL_GEMDOS_LIBNAME "gemdos"
 LUAMOD_API int (luaopen_gemdos)(lua_State *L);
 
 #endif
